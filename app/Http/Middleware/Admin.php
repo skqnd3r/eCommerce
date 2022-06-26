0031960779841b3a619209b5ae39e6c150b5f2d1
skqnd3r <alexandre.adm3@gmail.com>
2022-06-26 05:24:19 -0500

    seeder setup

--- app/Http/Middleware/Admin.php
+++ app/Http/Middleware/Admin.php
@@ -18,7 +18,7 @@ class Admin
     public function handle(Request $request, Closure $next)
     {
         if (Auth::check()) {
-            if (Auth::user()->admin != 1) {
+            if (Auth::user()->admin != 0) {
                 return $next($request);
             }
         }
--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -16,19 +16,17 @@ class ProductSeeder extends Seeder
     public function run()
     {
         $i=0;
-        $list=array("Aqualight","Cubico","Eda","Lantana","Lechuza");
-        while($i != 20){
-            foreach($list as $item){
-                DB::table('products')->insert([
-                    'title' => $item." (".$i.")",
-                    'price' => random_int(15,100),
-                    'img' => '/resources/products/'.$item.".jpg",
-                    'alt' => $item.$i,
-                    'description' => Str::random(50),
-                    'quantity' => random_int(1,15),                
-                ]);
-                $i++;
-            }
+        // $list=array("Aqualight","Cubico","Eda","Lantana","Lechuza");
+        $list=array("Lantana","Nelly","Aqualight","Orchidee","Cubico","Spathiphyllum");
+        foreach($list as $item){
+            DB::table('products')->insert([
+                'title' => $item,
+                'price' => random_int(15,100),
+                'img' => '/resources/products/'.$item.".jpg",
+                'alt' => $item,
+                'description' => "Lorem ipsum dolor sit amet. Nam consequatur quia est atque perferendis aut odio nihil. Et incidunt consequatur eos dolore mollitia ad nisi incidunt et rerum voluptatum et unde minima.",
+                'quantity' => random_int(1,15),                
+            ]);
         }
     }
 }
